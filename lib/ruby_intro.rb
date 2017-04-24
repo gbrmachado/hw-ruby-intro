3ef7b2d826d3120f40dd94fe7d305befb73b34a9
Goma <gbrmachado@gmail.com>
2017-04-23 21:51:26 -0300

    HW 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,55 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.reduce(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum(arr.max(2))
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.empty?
+    false
+  else
+    (arr.empty? && n.zero?) || arr.permutation(2).any? { |a, b| a + b == n }
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[^aeiou\W]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  !s.empty? && s.chars.all? {|x| x =~ /[01]/} && s.to_i(2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    if isbn.empty?
+      raise ArgumentError, "ISBN is empty"
+    end
+    if price <= 0
+      raise ArgumentError, "Price negative or zero"
+    end
+
+    @isbn = isbn
+    @price = price
+  end
+
+  attr_accessor :isbn, :price
+
+  def price_as_string
+    "$%.2f" % @price
+  end
+
+
 end
